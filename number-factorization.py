8dad13a9481f7270b1c9d59ef70b85ca3ddf7799
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-10 16:49:01 +0700

    main

---
+++ number-factorization.py
@@ -0,0 +1,17 @@
+# hello my name is akmal this is my mini project for my experiences
+# im from indonesia, Please forgive me if my English is not good. 
+
+# WHAT'S THE NUMBER FACTORIZATION ?
+
+# Bayang kan Faktorisasi seperti memecah coklat batangan menjadi kepingan coklat yang lebih keil
+# faktorisasi hakikatnya adalah mecaru bilangan yang lebih kecil yang jika dikalikan akan menghasilkan bilangan aslinya
+
+# EXAMPLE
+
+# if 12 adalah bilangan genap maka berapa dikalikan berapa yang hasilnya adalah 12 ?
+
+# 1 * 12 = 12
+# 3 * 4 = 12     
+# 2 * 6 = 12 
+
+#  jadi beberapakombinasi angka yang dikalikan ini merupakan contoh dari operasi Faktorisasi
