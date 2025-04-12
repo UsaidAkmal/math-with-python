c6f84155b2ab4428579a6a8a3cb7b24db0f348ab
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-12 11:21:30 +0700

    main

--- factorization/factorization_game.py
+++ factorization/factorization_game.py
@@ -10,3 +10,15 @@ print ('''
  the origin of number, can you play with this function?
 
 ''')
+
+import random # untuk mengimport modul random dari library python
+
+def tebak_bilangan():
+    # ini merupakan function untuk kita dapat menebak bilangan dari 1-50 untuk mencari factor terbesar dari beberapa factor kecil yang mempengaruhi.
+    secret_number = random.randint(1, 50)
+
+    # find the factors (berdasarkan secret number yang sudah ditentukan oleh modul random dari python)
+    factor = find_factors(secret_number)
+
+    # fungsi untuk tidak menampilkan faktor terbesar yaitu bilangan itu sendiri (jawabanya).
+    clue = factor
