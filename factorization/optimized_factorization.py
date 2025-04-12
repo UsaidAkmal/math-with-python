cd52469a78e0e53416fe73c0937efaa1ce9e558d
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-12 10:52:32 +0700

    main

--- factorization/optimized_factorization.py
+++ factorization/optimized_factorization.py
@@ -17,16 +17,30 @@ import math
 n = 15
 
 def find_factor_optimized(n):
-    factor = []
+    factor = [] # adalah variabel yang menampung hasil fungsi berupa faktor yang sudah di temukan 
+
     # hanya merunut sampai akar kuadrat dari n
     for i in range(1, int(math.sqrt(n)) + 1):
+
         # jika i habis membagi n maka masukan menggunakan append kedalam var factor
         if n % i == 0:
-            factor.append(i)
-            # 
-    return factor
+            factor.append(i) # memasukan faktor kecil kedalam var factor
+
+            # Menambahkan pasangan untuk setiap factor
+            if i != n // i: # // adalah pembagian bilangan bulat, menghasilkan hasil pembagian tanpa desimal, n // i menghasilkan bilangan yang jika dikalikan dengan i, akan menghasilkan n.
+
+                # kemudian kita akan memeriksa apakah i tidak sama (!=) dengan n // i untuk menghindari menambahkan faktor yang sama dua kali.
+
+                # contoh untuk n-25, jika i adalah 5, maka n // i=5, karena 5=5, dan 5 akan dii simpan hanya sekali pada var factor.
+
+                # setelah menemukan pasangan dari factor yang sudah kita input, selanjutnya kita akan menyimpannya kedalam var factor kembali, menggunakan append seperti factor pertamannya.
+                factor.append(n // i)
+
+        # Urutkan factor untuk out-put yang lebih rapih
+        factor.sort()
+    return factor # kembalikan nilai var factor, lalu cetak menggunakan print 
 
 
-print(find_factor(24))
+print(find_factor_optimized(n))
 
 
