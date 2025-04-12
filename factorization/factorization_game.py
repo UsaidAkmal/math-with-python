49ad56d51609817313368fba611c41a6d6850ed6
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-12 11:03:27 +0700

    main

--- factorization/factorization_game.py
+++ factorization/factorization_game.py
@@ -7,6 +7,6 @@ print ('''
  | | | (_| | (__| || (_) | |  | |/ / (_| | |_| | (_) | | | |
  |_|  \__,_|\___|\__\___/|_|  |_/___\__,_|\__|_|\___/|_| |_|
   
- the origin of number, can you play with this function ?
+ the origin of number, can you play with this function?
 
 ''')
