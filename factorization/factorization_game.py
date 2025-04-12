22f60b172f7291a9e20133db1b2a1e9263d458e7
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-12 11:39:46 +0700

    first_try

--- factorization/factorization_game.py
+++ factorization/factorization_game.py
@@ -21,4 +21,18 @@ def tebak_bilangan():
     factor = find_factors(secret_number)
 
     # fungsi untuk tidak menampilkan faktor terbesar yaitu bilangan itu sendiri (jawabanya).
-    clue = factor
+    clue = factor[: -1]
+
+    # berikut adalah fungsi untuk menampilak faktor2 kecil yang menjadi pengaruh dari jawaban (Factor aslinya).
+    print(f"I think a numbers, the factors is : {clue}") # print akan menampilakn var clue yang sudah di dapatkan dari hasil var secret number yang akan menjadi faktor2 kecil dari jawaban asli.
+
+    # saya akan memberi 3 kali kesempatan untuk menebak, jika salah maka akan mengeluarkan print yang berisi bahwa jawaban yang anda masukan salah.
+    for trials in range(3):
+        tebakan = int(input(f"Tebakan ke-{trials+1}: ")) # ingat memang harus di + 1, jika tidak maka percobaan akan = - 1 (secara default pemrograman)
+        if tebakan == secret_number:
+            print(f"Congrats! Your answer is true, the secret number is: {secret_number}") # akan mencetak secret number, karena tebakan sesuai dengan factor terbesar dari factor2 kecil (jawabanya).
+            return
+        else: # jika salah maka cetak string berikut.
+            print("Shit! your answer is false, try again.")
+
+        print(f"OOh nooo, your trials all done, the secret number is: {secret_number}, see you leter!")
