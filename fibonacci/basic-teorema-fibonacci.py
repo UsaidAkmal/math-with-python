9390defebeadd54aec4ea2a788a4d47f8a378cf8
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-16 19:46:30 +0700

    main

---
+++ fibonacci/basic-teorema-fibonacci.py
@@ -0,0 +1,7 @@
+# SIMPLE IMPLEMENTATION FIBONACCI TEOREMA WITH MY SNAKE AKA Python
+
+# Bayangkan Anda sedang berjalan di taman, mengamati kelopak bunga yang mekar dengan sempurna. Atau mungkin Anda memperhatikan pola spiral pada kerang laut. Tanpa Anda sadari, ada sebuah pola matematika kuno yang hadir dalam keindahan alam di sekitar Anda. Pola ini bernama deret Fibonacci—suatu rangkaian bilangan sederhana namun memiliki kedalaman yang luar biasa.
+Deret Fibonacci pertama kali diperkenalkan oleh matematikawan Italia bernama Leonardo Pisano, yang lebih dikenal dengan nama Fibonacci, pada abad ke-13. Dalam bukunya "Liber Abaci" (1202), ia menyajikan sebuah teka-teki tentang perkembangbiakan kelinci yang melahirkan konsep matematika yang hingga kini mempesona para ilmuwan, seniman, dan bahkan para pengamat alam.
+
+
+def fibonacci_recursive(n): # mendefinisikan variabel fungsi yang akan di gunakan dan di buat yaitu fibonacci_recursive
