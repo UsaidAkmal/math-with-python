a531f1ab22e07975685f87ee6f3cfd46fabac0f1
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-17 10:11:31 +0700

    perhitungan

--- fibonacci/iterartif-methode.py
+++ fibonacci/iterartif-methode.py
@@ -22,8 +22,18 @@ def fibonacci_iteratif(n): # mendefinisikan fungsi yang akan di buat
 
     a, b = 0, 1 # disini a dan b, a adalah f(0)=0 dan b adalah f(1)=1
 
-    # mulai melakukan iterasi menggunakan f(1)=1 dan f(n-1) untuk 
+    # mulai melakukan iterasi menggunakan f(1)=1 dan f(n-1) "deret fibonacci itu sendiri" untuk mendapatkan nilai yang akurat untuk f(n) 
 
+    # lalu kita akan mengaitkannya menggunakan loop dari deret 1 hingga n.
+
+    for i in range(1, n): # untuk i dalam deret 1 hingga n
+
+        # simpan nilai a sebelum di ubah untuk melakukan efisiensi perhitungan. agar tidak terjadi proses komputasi yang berlebih. 
+        temp = a
+
+        # mulai melakukan perhitungan vvariabel utama yaitu f(0) dan f(1) dengan melalui temporary variabel yang sudah tersimpan. untuk memanfaatkan data yang sudah di simpan. 
+        a = b 
+        b = temp + b
 
 
 
