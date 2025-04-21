51c5703c6c0aa826836b7358efaf3bfd02ce5e25
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-21 07:38:59 +0700

    main

---
+++ fibonacci/matrix-exponentiation.py
@@ -0,0 +1,4 @@
+# UNTUK INPUT YANG LEBIH BESAR LAGI ITERATI DAN MEMOIZATION SAJA TIDAK CUKUP,DAN BISA MENJADI LAMBAT. dalam kasus inputan yang jauh jauh llebih besar kita dapat melakukan pendekatan matematika yang jauh jauh lebih canggih yang menggunakan pemangkatan matriks.
+
+
+
