d0ceb6a2b507d605585602ece632f2b25d8aedbf
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-23 14:26:00 +0700

    progres

--- fibonacci/matrix-exponentiation.py
+++ fibonacci/matrix-exponentiation.py
@@ -3,3 +3,4 @@
 # Dalam dunia komputasi, efisiensi adalah segalanya. Kita bisa menulis algoritma Fibonacci dengan cara naïf (rekursif biasa), lalu meningkat ke iteratif atau memakai memoization (top-down dengan cache). Tapi, saat kita ingin menghitung Fibonacci ke-1.000.000, bahkan pendekatan tersebut akan kelimpungan. Di sinilah pemangkatan matriks (matrix exponentiation) masuk sebagai senjata pamungkas dari dunia matematika diskret (adalah displin ilmu matematika yang membahas objek2 terpisah atau diskrit) dan aljabar linear (cabang matematika yang fokus pada mempelajari sistem persamaan linear, vektor, matriks, dan transformasi linear) untuk menyelamatkan waktu komputasi secara drastis.
 
 # APA ITU PEMANGKATAN MATRIKS ?
+
