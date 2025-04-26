b5fcc21ca0268473329425726f614bac0ff6f5db
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-04-26 23:50:01 +0700

    main

--- fibonacci/matrix-exponentiation.py
+++ fibonacci/matrix-exponentiation.py
@@ -4,3 +4,30 @@
 
 # APA ITU PEMANGKATAN MATRIKS ?
 
+# pemangkatan matriks merupakan sebuah perkalian matriks menggunakan bilangan berpangkat, yang dimana matriks tersebut jumlah barisnya sama dengan jumlah kolomnya, atau yang di sebut dengan MATRIKS KUADRAT. yang natinya matriks kuadrat tersebut akan dikalikan dengan dirinya sendiri dengan rasio bilangan pangkatnya.
+
+# Contoh :
+
+# A = [1 2]
+#     [3 4]
+
+# hitungalah metriks A pangkat 2 (A²):
+
+# maka [1 2] akan dikalikan dengan [1 2]
+#      [3 4] akan dikalikan dengan [3 4]
+
+# Baris pertama × kolom pertama: [1 2] * [1]
+#                                        [3] maka : 1 * 1 + 2 * 3 = 1 + 6 = 7
+
+# Baris pertama x kolom kedua: [1 2] * [2]
+#                                      [4] maka : 1 * 2 + 2 * 4 = 2 + 8 = 10
+
+# Baris kedua x kolom pertama: [3 4] * [1]
+#                                      [3] maka : 3 * 1 + 4 * 3 = 3 + 12 = 15 
+
+# Baris kedua x kolom kedua : [3 4] * [2]
+#                                     [4] maka : 3 * 2 + 4 * 4 = 6 + 16 = 22
+
+# 
+
+
